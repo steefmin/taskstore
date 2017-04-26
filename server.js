0bad582cf5aac54682a35685b2dc1417da936291
Steef Min <steefmin@gmail.com>
2017-04-26 15:21:25 +0200

    adds elsje customization

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var router = express.Router()              // get an instance of the express Rou
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function (req, res) {
-  res.json({message: 'hooray! welcome to our api!'})
+  res.json({message: 'hooray! welcome to the Elsje taskstore api!'})
 })
 
 // REGISTER OUR ROUTES -------------------------------
